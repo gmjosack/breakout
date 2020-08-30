cc72eba582504a20ce2210d027766b914c501a0c
Gary M. Josack <gary@byoteki.com>
2020-08-30 16:04:23 -0700

    Updated paddle and ball to use sprites

--- src/breakout/__init__.py
+++ src/breakout/__init__.py
@@ -5,58 +5,59 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Breakout"
 
+MOVEMENT_SPEED = 8
+
 PADDLE_WIDTH = 150
 PADDLE_HEIGHT = 20
 
-MOVEMENT_SPEED = 8
 
-BALL_DIAMETER = 7
+class Paddle(arcade.SpriteSolidColor):
 
 
-@dataclass
-class Paddle:
-    center_x: float
-    center_y: float
-    change_x: float = 0
-    change_y: float = 0
-    width: float = 150
-    height: float = 20
-    color: arcade.Color = arcade.color.ANTI_FLASH_WHITE
+    def __init__(self,
+        center_x, center_y,
+        width=PADDLE_WIDTH, height=PADDLE_HEIGHT,
+        color=arcade.color.ANTI_FLASH_WHITE,
+    ):
+        super().__init__(width=width, height=height, color=color)
+        self.center_x = center_x
+        self.center_y = center_y
 
-    def draw(self):
-        arcade.draw_rectangle_filled(
-            center_x = self.center_x,
-            center_y = self.center_y,
-            width = self.width,
-            height = self.height,
-            color = self.color,
-        )
-
-    def move(self):
+    def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
+        if self.left < 0:
+            self.left = 0
+        elif self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH - 1
 
-@dataclass
-class Ball:
-    center_x: float
-    center_y: float
-    change_x: float = 0
-    change_y: float = 0
-    radius: float = 7
-    color: arcade.Color = arcade.color.ANTI_FLASH_WHITE
 
-    def draw(self):
-        arcade.draw_circle_filled(
-            center_x = self.center_x,
-            center_y = self.center_y,
-            radius = self.radius,
-            color = self.color,
-        )
 
-    def move(self):
+class Ball(arcade.SpriteCircle):
+    def __init__(self,
+        center_x, center_y, change_x=0, change_y=0,
+        radius=7, color=arcade.color.ANTI_FLASH_WHITE,
+    ):
+        super().__init__(radius=radius, color=color)
+        self.change_x = change_x
+        self.change_y = change_y
+        self.center_x = center_x
+        self.center_y = center_y
+
+    def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+
+        if self.left < 0:
+            self.change_x = -self.change_x
+        elif self.right >= SCREEN_WIDTH:
+            self.change_x = -self.change_x
+
+        if self.bottom < 0:
+            self.change_y = -self.change_y
+        elif self.top >= SCREEN_HEIGHT:
+            self.change_y = -self.change_y
 
 
 class Breakout(arcade.Window):
@@ -74,6 +75,8 @@ class Breakout(arcade.Window):
         self.ball = Ball(
             center_x = SCREEN_WIDTH / 2,
             center_y = PADDLE_HEIGHT * 3,
+            change_x = 5,
+            change_y = 5,
         )
 
 
@@ -83,7 +86,8 @@ class Breakout(arcade.Window):
         self.ball.draw()
 
     def on_update(self, delta_time):
-        self.paddle.move()
+        self.paddle.update()
+        self.ball.update()
 
 
     def on_key_press(self, key, modifiers):
